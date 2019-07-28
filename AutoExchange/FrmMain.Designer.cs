17993b0b4789dc6c18eff786b4ba2369d2befc45
dzampa <diego_zamparo@hotmail.com>
2019-07-28 18:47:38 -0300

    Add project files.

---
+++ AutoExchange/FrmMain.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace AutoExchange
+{
+    partial class AutoExchange
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AutoExchange));
+            this.btnRun = new System.Windows.Forms.Button();
+            this.txbResult = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbAPIKey = new System.Windows.Forms.TabPage();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txbSecretKey = new System.Windows.Forms.TextBox();
+            this.txbAPIKey = new System.Windows.Forms.TextBox();
+            this.tpUseSetting = new System.Windows.Forms.TabPage();
+            this.chbAutoNeg = new System.Windows.Forms.CheckBox();
+            this.chbUseCurrBTC = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txbFeeTax = new System.Windows.Forms.TextBox();
+            this.txbInBTC = new System.Windows.Forms.TextBox();
+            this.txbConGanAbov = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbController = new System.Windows.Forms.TabControl();
+            this.tbAPIKey.SuspendLayout();
+            this.tpUseSetting.SuspendLayout();
+            this.tbController.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(713, 34);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 97);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "Run!!";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // txbResult
+            // 
+            this.txbResult.Location = new System.Drawing.Point(12, 169);
+            this.txbResult.Multiline = true;
+            this.txbResult.Name = "txbResult";
+            this.txbResult.ReadOnly = true;
+            this.txbResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txbResult.Size = new System.Drawing.Size(776, 269);
+            this.txbResult.TabIndex = 99999;
+            this.txbResult.TextChanged += new System.EventHandler(this.TxbResult_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Results";
+            // 
+            // tbAPIKey
+            // 
+            this.tbAPIKey.Controls.Add(this.label7);
+            this.tbAPIKey.Controls.Add(this.label8);
+            this.tbAPIKey.Controls.Add(this.txbSecretKey);
+            this.tbAPIKey.Controls.Add(this.txbAPIKey);
+            this.tbAPIKey.Location = new System.Drawing.Point(4, 22);
+            this.tbAPIKey.Name = "tbAPIKey";
+            this.tbAPIKey.Padding = new System.Windows.Forms.Padding(3);
+            this.tbAPIKey.Size = new System.Drawing.Size(687, 97);
+            this.tbAPIKey.TabIndex = 2;
+            this.tbAPIKey.Text = "API KEY";
+            this.tbAPIKey.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 42);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 13);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Secret Key";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(3, 3);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "API Key";
+            // 
+            // txbSecretKey
+            // 
+            this.txbSecretKey.Location = new System.Drawing.Point(3, 58);
+            this.txbSecretKey.Name = "txbSecretKey";
+            this.txbSecretKey.Size = new System.Drawing.Size(425, 20);
+            this.txbSecretKey.TabIndex = 18;
+            this.txbSecretKey.Text = "Secret";
+            // 
+            // txbAPIKey
+            // 
+            this.txbAPIKey.Location = new System.Drawing.Point(3, 19);
+            this.txbAPIKey.Name = "txbAPIKey";
+            this.txbAPIKey.Size = new System.Drawing.Size(425, 20);
+            this.txbAPIKey.TabIndex = 17;
+            this.txbAPIKey.Text = "API";
+            // 
+            // tpUseSetting
+            // 
+            this.tpUseSetting.Controls.Add(this.chbAutoNeg);
+            this.tpUseSetting.Controls.Add(this.chbUseCurrBTC);
+            this.tpUseSetting.Controls.Add(this.label4);
+            this.tpUseSetting.Controls.Add(this.label6);
+            this.tpUseSetting.Controls.Add(this.txbFeeTax);
+            this.tpUseSetting.Controls.Add(this.txbInBTC);
+            this.tpUseSetting.Controls.Add(this.txbConGanAbov);
+            this.tpUseSetting.Controls.Add(this.label5);
+            this.tpUseSetting.Location = new System.Drawing.Point(4, 22);
+            this.tpUseSetting.Name = "tpUseSetting";
+            this.tpUseSetting.Padding = new System.Windows.Forms.Padding(3);
+            this.tpUseSetting.Size = new System.Drawing.Size(687, 97);
+            this.tpUseSetting.TabIndex = 0;
+            this.tpUseSetting.Text = "Usual Setting";
+            this.tpUseSetting.UseVisualStyleBackColor = true;
+            // 
+            // chbAutoNeg
+            // 
+            this.chbAutoNeg.AutoSize = true;
+            this.chbAutoNeg.Location = new System.Drawing.Point(142, 61);
+            this.chbAutoNeg.Name = "chbAutoNeg";
+            this.chbAutoNeg.Size = new System.Drawing.Size(138, 17);
+            this.chbAutoNeg.TabIndex = 14;
+            this.chbAutoNeg.Text = "Automatic Negociations";
+            this.chbAutoNeg.UseVisualStyleBackColor = true;
+            // 
+            // chbUseCurrBTC
+            // 
+            this.chbUseCurrBTC.AutoSize = true;
+            this.chbUseCurrBTC.Location = new System.Drawing.Point(178, 21);
+            this.chbUseCurrBTC.Name = "chbUseCurrBTC";
+            this.chbUseCurrBTC.Size = new System.Drawing.Size(177, 17);
+            this.chbUseCurrBTC.TabIndex = 13;
+            this.chbUseCurrBTC.Text = "Use BTC Funds Acount (Online)";
+            this.chbUseCurrBTC.UseVisualStyleBackColor = true;
+            this.chbUseCurrBTC.CheckedChanged += new System.EventHandler(this.ChbUseCurrBTC_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 3);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Fee Tax%";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(66, 3);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(106, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Initial Amount of BTC";
+            // 
+            // txbFeeTax
+            // 
+            this.txbFeeTax.Location = new System.Drawing.Point(6, 19);
+            this.txbFeeTax.Name = "txbFeeTax";
+            this.txbFeeTax.Size = new System.Drawing.Size(54, 20);
+            this.txbFeeTax.TabIndex = 3;
+            this.txbFeeTax.Text = "0.1";
+            // 
+            // txbInBTC
+            // 
+            this.txbInBTC.Location = new System.Drawing.Point(66, 19);
+            this.txbInBTC.Name = "txbInBTC";
+            this.txbInBTC.Size = new System.Drawing.Size(106, 20);
+            this.txbInBTC.TabIndex = 5;
+            this.txbInBTC.Text = "0.00244286";
+            // 
+            // txbConGanAbov
+            // 
+            this.txbConGanAbov.Location = new System.Drawing.Point(6, 58);
+            this.txbConGanAbov.Name = "txbConGanAbov";
+            this.txbConGanAbov.Size = new System.Drawing.Size(130, 20);
+            this.txbConGanAbov.TabIndex = 4;
+            this.txbConGanAbov.Text = "1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 42);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(130, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Consider Gain Above of %";
+            // 
+            // tbController
+            // 
+            this.tbController.Controls.Add(this.tpUseSetting);
+            this.tbController.Controls.Add(this.tbAPIKey);
+            this.tbController.Location = new System.Drawing.Point(12, 12);
+            this.tbController.Name = "tbController";
+            this.tbController.SelectedIndex = 0;
+            this.tbController.Size = new System.Drawing.Size(695, 123);
+            this.tbController.TabIndex = 100000;
+            // 
+            // AutoExchange
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbController);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txbResult);
+            this.Controls.Add(this.btnRun);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AutoExchange";
+            this.Text = "AutoExchange";
+            this.tbAPIKey.ResumeLayout(false);
+            this.tbAPIKey.PerformLayout();
+            this.tpUseSetting.ResumeLayout(false);
+            this.tpUseSetting.PerformLayout();
+            this.tbController.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.TextBox txbResult;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TabPage tbAPIKey;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txbSecretKey;
+        private System.Windows.Forms.TextBox txbAPIKey;
+        private System.Windows.Forms.TabPage tpUseSetting;
+        private System.Windows.Forms.CheckBox chbUseCurrBTC;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txbFeeTax;
+        private System.Windows.Forms.TextBox txbInBTC;
+        private System.Windows.Forms.TextBox txbConGanAbov;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TabControl tbController;
+        private System.Windows.Forms.CheckBox chbAutoNeg;
+    }
+}
+
---
+++ AutoExchange/FrmMain.cs
@@ -0,0 +1,348 @@
+﻿using BinanceExchange.API.Client;
+using BinanceExchange.API.Enums;
+using BinanceExchange.API.Extensions;
+using BinanceExchange.API.Models.Request;
+using BinanceExchange.API.Models.Response;
+using BinanceExchange.API.Models.Response.Error;
+using log4net;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace AutoExchange
+{
+    public partial class  AutoExchange : Form
+    {
+        public AutoExchange()
+        {
+            InitializeComponent();
+        }
+
+        public async void Teste()
+        {
+           
+            StringBuilder txt;
+            string apiKey;
+            string secretKey;
+
+            decimal startCurrence;
+            decimal fee;
+
+            string m1;
+            string m2;
+            decimal soma1;
+            decimal soma2;
+            string p1;
+            string p2;
+            long timestamp1;
+            long timestamp2;
+
+            bool Valid;
+            bool Auto;
+
+            try
+            {
+
+                apiKey = txbAPIKey.Text.Trim();
+                secretKey = txbSecretKey.Text.Trim();
+                Auto = chbAutoNeg.Checked;
+                txt = new StringBuilder();
+
+                txt.AppendLine("--------------------------");
+                txt.AppendLine("BinanceExchange API - Tester");
+                txt.AppendLine("--------------------------");
+
+                txbResult.Text += txt;
+
+                //Building a test logger
+                var ProgramLogger = LogManager.GetLogger(typeof(AutoExchange));
+                ProgramLogger.Debug("Logging Test");
+
+                //Initialise the general client client with config
+                var client = new BinanceClient(new ClientConfiguration()
+                {
+                    ApiKey = apiKey,
+                    SecretKey = secretKey,
+                    Logger = ProgramLogger,
+                });
+
+                txt = new StringBuilder();
+                txt.AppendLine("Get BTC Funds: " + chbUseCurrBTC.Checked);
+                txt.AppendLine("Automatic Negociations: " + Auto);
+                txt.AppendLine("Interacting with Binance...");
+                txbResult.Text += txt;
+
+                // Set TimestampOffset for ajust hour with server
+                txbResult.AppendText("Checking the time with the server...\r\n");
+
+#if NETSTANDARD2_0
+                timestamp1 = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+#else
+                timestamp1 = DateTime.UtcNow.ConvertToUnixTime();
+#endif
+
+                var serverTime = await client.GetServerTime();
+
+                timestamp2 = serverTime.ServerTime.ConvertToUnixTime();
+
+                client.TimestampOffset = System.TimeSpan.FromMilliseconds(timestamp2-timestamp1);
+
+                if (chbUseCurrBTC.Checked)
+                {
+                    txbResult.AppendText("Get funds from BTC Funds account (Online)...\r\n");
+
+                    // Get account information
+                    var accountInformation = await client.GetAccountInformation(3500);
+
+                    var balances = accountInformation.Balances;
+
+                    foreach (var balance in balances)
+                    {
+                        if (balance.Asset == "BTC")
+                        {
+                            txbInBTC.Text = balance.Free.ToString();
+                            txbResult.AppendText("Found BTC amout: " + balance.Free + "\r\n");
+                        }
+                    } 
+
+                }
+
+                var bnb = await client.GetDailyTicker24();
+
+                if (bnb.Count <= 0)
+                    return;
+
+                startCurrence = 0;            
+                fee = Convert.ToDecimal(txbFeeTax.Text.Trim())/100;
+
+                //Só termina com BTC
+                foreach (var par in bnb)
+                {
+                    Valid = true;
+                    startCurrence = Convert.ToDecimal(txbInBTC.Text.Trim());
+                    
+                    m1 = "";
+                    m2 = "";
+                    if (par.symbol.Length == 5)
+                    {
+                        m1 = par.symbol.Substring(0, 2);
+                        m2 = par.symbol.Substring(2, 3);
+                    }
+                    else if (par.symbol.Length == 6)
+                    {
+                        m1 = par.symbol.Substring(0, 3);
+                        m2 = par.symbol.Substring(3, 3);
+                    }
+                    else if (par.symbol.Length == 7)
+                    {
+                        m1 = par.symbol.Substring(0, 4);
+                        m2 = par.symbol.Substring(4, 3);
+                    }
+                    else if (par.symbol.Length == 8)
+                    {
+                        m1 = par.symbol.Substring(0, 5);
+                        m2 = par.symbol.Substring(5, 3);
+                    }
+                    else if (par.symbol.Length == 9)
+                    {
+                        if (par.symbol.StartsWith("BCHABC"))
+                        {
+                            m1 = par.symbol.Substring(0, 6);
+                            m2 = par.symbol.Substring(6, 3);
+                        }
+                        else
+                        {
+                            m1 = par.symbol.Substring(0, 5);
+                            m2 = par.symbol.Substring(5, 4);
+                        }
+                    }
+                    else if (par.symbol.Length == 10)
+                    {
+                        m1 = par.symbol.Substring(0, 6);
+                        m2 = par.symbol.Substring(6, 4);
+                    }
+
+                    if (par.AskPrice == 0)
+                        continue;
+
+                    soma1 = Math.Round(startCurrence / par.AskPrice, 8);
+                    if (par.askQty <= soma1)
+                        Valid = false;
+                    soma1 -= Math.Round(soma1 * fee,8);                    
+
+                    var pares = bnb.Where(x => x.symbol.StartsWith(m1) && !x.symbol.Contains(par.symbol) && !x.symbol.Contains("USD")).ToList();
+
+                    foreach (var parMoeda1 in pares)
+                    {
+                        txt = new StringBuilder();
+                        txt.AppendLine("==============================================================================");
+                        txt.AppendLine("Exchange: " + par.symbol + " - Buy Price: " + par.AskPrice + m2 + " - Total Coin: " + soma1 + m1);
+
+                        soma2 = 0;
+                        if (par.AskPrice == 0)
+                            continue;
+                        if (parMoeda1.AskPrice > 1)
+                        {
+                            soma2 = Math.Round(soma1 / parMoeda1.AskPrice, 8);
+                        }
+                        else
+                        {
+                            soma2 = Math.Round(soma1 * parMoeda1.AskPrice, 8);
+                        }
+
+                        if (parMoeda1.askQty <= soma2)
+                            Valid = false;
+
+                        soma2 -= Math.Round(soma2 * fee ,8);
+                        txt.AppendLine("Exchange: " + parMoeda1.symbol + " - Buy Price: " + parMoeda1.BidPrice + m1 + " - Total Coin: " + soma2 + parMoeda1.symbol.Replace(m1,""));
+
+                        p1 = "";
+                        p2 = "";
+                        if (parMoeda1.symbol.Length == 5)
+                        {
+                            p1 = parMoeda1.symbol.Substring(0, 2);
+                            p2 = parMoeda1.symbol.Substring(2, 3);
+                        }
+                        else if (parMoeda1.symbol.Length == 6)
+                        {
+                            p1 = parMoeda1.symbol.Substring(0, 3);
+                            p2 = parMoeda1.symbol.Substring(3, 3);
+                        }
+                        else if (parMoeda1.symbol.Length == 7)
+                        {
+                            p1 = parMoeda1.symbol.Substring(0, 4);
+                            p2 = parMoeda1.symbol.Substring(4, 3);
+                        }
+                        else if (parMoeda1.symbol.Length == 8)
+                        {
+                            p1 = parMoeda1.symbol.Substring(0, 5);
+                            p2 = parMoeda1.symbol.Substring(5, 3);
+                        }
+                        else if (parMoeda1.symbol.Length == 9)
+                        {
+                            if (parMoeda1.symbol.StartsWith("BCHABC"))
+                            {
+                                p1 = parMoeda1.symbol.Substring(0, 6);
+                                p2 = parMoeda1.symbol.Substring(6, 3);
+                            }
+                            else
+                            {
+                                p1 = parMoeda1.symbol.Substring(0, 5);
+                                p2 = parMoeda1.symbol.Substring(5, 4);
+                            }
+                        }
+                        else if (parMoeda1.symbol.Length == 10)
+                        {
+                            p1 = parMoeda1.symbol.Substring(0, 6);
+                            p2 = parMoeda1.symbol.Substring(6, 4);
+                        }
+
+                        var parMoedaBTC = bnb.First(x => x.symbol.Contains(m1 == p1 ? p2 : p1 + "BTC"));
+                        if (parMoedaBTC.AskPrice == 0)
+                            continue;
+                        
+                        var final = Math.Round(soma2 * parMoedaBTC.AskPrice, 8);
+                        if (parMoedaBTC.askQty <= final)
+                            Valid = false;
+                        final -= Math.Round(final * fee,8);
+
+                        txt.AppendLine("Exchange: " + parMoedaBTC.symbol + " - Sell Price: " + parMoedaBTC.BidPrice + p2 + " - Total Coin: " + final + m2);
+                        var Sp = Math.Round((((final * 100) / startCurrence) - 100), 2);
+                        if (final >= startCurrence && Sp >= Convert.ToDecimal(txbConGanAbov.Text.Trim()))
+                        {
+                            var msg = Sp + "% *** 1º Step: " + par.symbol + " - 2º Step: " + parMoeda1.symbol + " - 3º Step: " + parMoedaBTC.symbol;
+                            txt.AppendLine(msg);
+                            txt.AppendLine("GAIN!!!!!!!!!!! Start: " + startCurrence + " | Stop: "+ final + " | Valid Transaction: " + Valid);
+                            txt.AppendLine("==============================================================================");                            
+                            txbResult.Text += txt;
+                            txbInBTC.Text = Convert.ToString( final); 
+                            if (Valid && Auto)
+                            {
+                                var createOrder1 = await client.CreateTestOrder(new CreateOrderRequest()
+                                {                                    
+                                    Price = par.AskPrice,
+                                    Quantity = Math.Round(soma1, 8),
+                                    Side = OrderSide.Buy,
+                                    Symbol = par.symbol,
+                                    Type = OrderType.LimitMaker,
+                                });
+
+
+                               var createOrder2= await client.CreateTestOrder(new CreateOrderRequest()
+                               {                                  
+                                   Price = parMoeda1.BidPrice,
+                                   Quantity = Math.Round(soma1, 8),
+                                   Side = OrderSide.Sell,
+                                   Symbol = parMoeda1.symbol,
+                                   Type = OrderType.LimitMaker,
+                               });
+
+                               var createOrder3 = await client.CreateTestOrder(new CreateOrderRequest()
+                               {                                   
+                                   Price = parMoedaBTC.BidPrice,
+                                   Quantity = Math.Round(soma2, 8),
+                                   Side = OrderSide.Sell,
+                                   Symbol = parMoedaBTC.symbol,
+                                   Type = OrderType.LimitMaker,
+                               });
+                            }
+                        }
+
+                    }
+
+                    
+                }
+            }
+            catch (BinanceBadRequestException ex)
+            {
+                txbResult.AppendText(ex.ErrorDetails + "\r\n");
+            }
+            catch (BinanceServerException ex)
+            {
+                txbResult.AppendText(ex.ErrorDetails + "\r\n");
+            }
+            catch (BinanceTimeoutException ex)
+            {
+                txbResult.AppendText(ex.ErrorDetails + "\r\n");
+            }
+            catch (BinanceException ex)
+            {
+                txbResult.AppendText(ex.ErrorDetails + "\r\n");
+            }
+            catch (Exception ex)
+            {                
+                txbResult.AppendText(ex.Message + "\r\n");
+            }            
+        }       
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            txbResult.Text = "";
+            Teste();
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void ChbUseCurrBTC_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chbUseCurrBTC.Checked)
+                txbInBTC.Enabled = false;
+            else
+                txbInBTC.Enabled = true;
+            
+        }
+
+        private void TxbResult_TextChanged(object sender, EventArgs e)
+        {
+            txbResult.SelectionStart = txbResult.Text.Length;
+            txbResult.ScrollToCaret();
+            txbResult.Refresh();
+        }
+    }    
+
+}
